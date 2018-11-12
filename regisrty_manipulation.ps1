049f7226ca6702545ab265b35d770ed0de9ac862
Rasmus <44962623+nachoNil@users.noreply.github.com>
2018-11-12 10:01:29 +0200

    Add files via upload

---
+++ regisrty_manipulation.ps1
@@ -0,0 +1,13 @@
+﻿$regPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+$keysTo1 = @("EnableLUA")
+$keysTo0 = @("dontdisplaylastusername","Test")
+
+foreach ($key in $keysTo0) {
+    Set-ItemProperty $regPath -Name $key -Value "0"
+    #Write-Host "Value for key" $key "set to 0..."
+}
+
+foreach ($key in $keysTo1) {
+    Set-ItemProperty $regPath -Name $key -Value "1"
+    #Write-Host "Value for key" $key "set to 1..."
+}
