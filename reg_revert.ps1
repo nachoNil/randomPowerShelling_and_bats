ba229c53490fe7423fa3ba34666883ba3e69fe9e
Rasmus <44962623+nachoNil@users.noreply.github.com>
2018-11-12 12:18:04 +0200

    Update reg_revert.ps1

--- reg_revert.ps1
+++ reg_revert.ps1
@@ -1,13 +1,25 @@
-﻿$regPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+$regPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
 $keysTo1 = @("disablecad")
 $keysTo0 = @("PromptOnSecureDesktop","EnableLUA","dontdisplaylastusername","dontdisplaylockeduserid")
 
 foreach ($key in $keysTo0) {
-    Set-ItemProperty $regPath -Name $key -Value "0"
-    #Write-Host "Value for key" $key "set to 0..."
+    Try {
+        Set-ItemProperty $regPath -Name $key -Value "0"
+        Write-Host "Value for key" $key "set to 0..."
+    } 
+    Catch {
+        Write-Host "Value of key" $key "could not be modified..."    
+    }
 }
 
 foreach ($key in $keysTo1) {
-    Set-ItemProperty $regPath -Name $key -Value "1"
-    #Write-Host "Value for key" $key "set to 1..."
+    Try {
+        Set-ItemProperty $regPath -Name $key -Value "1"
+        Write-Host "Value for key" $key "set to 1..."
+    }
+    Catch {
+        Write-Host "Value of key" $key "could not be modified..."
+    }
 }
+
+Read-Host -Prompt "Press Enter to exit"
